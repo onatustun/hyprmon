03d8e06aec02e025d23ec6b70a54c72640649439
onatustun <git@onatustun.com>
2025-09-14 13:57:36 +0100

    feat: compat shell.nix, self titled package and default

--- nix/packages.nix
+++ nix/packages.nix
@@ -5,18 +5,23 @@
     self',
     ...
   }: {
-    packages.default = pkgs.buildGoModule {
-      pname = "hyprmon";
-      version = "0.0.8";
-      src = inputs.gitignore.lib.gitignoreSource ./..;
-      vendorHash = "sha256-D3hd5GN7I7sV/dSWj45cMn0oyKDHZ1rE26OWWU34lFU=";
+    packages = rec {
+      hyprmon = pkgs.buildGoModule {
+        pname = "hyprmon";
+        version = "0.0.8";
+        src = inputs.gitignore.lib.gitignoreSource ./..;
+        vendorHash = "sha256-D3hd5GN7I7sV/dSWj45cMn0oyKDHZ1rE26OWWU34lFU=";
 
-      meta = with lib; {
-        description = "TUI monitor configuration tool for Hyprland with visual layout, drag-and-drop, and profile management";
-        license = licenses.asl20;
-        mainProgram = "hyprmon";
-        platforms = platforms.linux;
+        meta = with lib; {
+          homepage = "https://github.com/erans/hyprmon";
+          description = "TUI monitor configuration tool for Hyprland with visual layout, drag-and-drop, and profile management";
+          license = licenses.asl20;
+          mainProgram = "hyprmon";
+          platforms = platforms.linux;
+        };
       };
+
+      default = hyprmon;
     };
 
     checks.default = self'.packages.default;
