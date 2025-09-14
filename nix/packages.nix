2db1cbe8949864bc045ada73bf3db7f015ffc457
onatustun <git@onatustun.com>
2025-09-14 22:05:41 +0100

    refactor: self

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,34 +1,37 @@
 {
-  self,
   lib,
+  self,
   inputs,
   ...
 }: let
-  version_rev =
-    if (self ? rev)
-    then (builtins.substring 0 8 self.rev)
+  inherit (lib) substring fileContents;
+
+  versionRev =
+    if self ? rev
+    then substring 0 8 self.rev
     else "dirty";
 
-  version = "${lib.fileContents ../VERSION}-${version_rev}-flake";
-  vendorHash = lib.fileContents ../go.mod.sri;
+  version = "${fileContents (self + "/VERSION")}-${versionRev}-flake";
+  vendorHash = fileContents (self + "/go.mod.sri");
 in {
   perSystem = {
     pkgs,
-    lib,
     self',
     ...
   }: {
-    packages = rec {
-      hyprmon = pkgs.buildGoModule {
+    packages = let
+      inherit (pkgs) buildGoModule;
+      inherit (inputs.gitignore.lib) gitignoreSource;
+    in rec {
+      hyprmon = buildGoModule {
         pname = "hyprmon";
-        inherit version;
-        src = inputs.gitignore.lib.gitignoreSource ./..;
-        inherit vendorHash;
-        env.CGO_ENABLED = 0;
+        inherit version vendorHash;
+        src = gitignoreSource ./..;
+        env.CGO_ENABLED = "0";
 
         meta = with lib; {
-          homepage = "https://github.com/erans/hyprmon";
           description = "TUI monitor configuration tool for Hyprland with visual layout, drag-and-drop, and profile management";
+          homepage = "https://github.com/erans/hyprmon";
           license = licenses.asl20;
           mainProgram = "hyprmon";
           platforms = platforms.linux;
